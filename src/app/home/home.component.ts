5d8cdc78e95f4fa6bd4238b4cb2af7973ac4eca9
roberta-fiume <robertaf898@gmail.com>
2021-07-01 10:33:17 +0100

    learning data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   homeTitle = "Welcome to Roberta homepage!";
+  myString = "I like chicken";
+  myBoolean = true;
+
+  alertMe(val: any) {
+    alert(val);
+  }
+
+  ninja = {
+    name: "Yoshi",
+    belt: "Black"
+  }
 
   constructor() { }
 
